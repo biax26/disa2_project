f1f56905872d0c1ced415d2b5a68a5dd5dc8cf94
Biagio Palmiero <palmiero.1938749@studenti.uniroma1.it>
2026-09-08 12:50:53 +0200

    Inizio del golden model

---
+++ model/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include "header.h"
+
+int main(int argc, char** argv)
+{
+//log2e in virgola mobile IEEE 754 sarebbe float log2e = 1.44269504, vogliamo portarlo in formato virgola fissa (fixed point) Q8.24 per farlo moltiplichiamo 1.44269504*2^24 e abbiamo 24204615 in decimale
+const int32_t log2e = 24204615;
+/* 
+Sezione per le flags
+
+*/
+
+
+
+
+	return 0;
+}
